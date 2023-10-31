56713a06b9eec86ecc4517fb7528c3b2779d6463
SungChanJoo <scjoo123@naver.com>
2023-10-31 17:22:03 +0900

    [feat] enemy 플레이어 추적 기능 추가
    Navmesh를 사용함

--- Assets/3.Script/IDamageable.cs
+++ Assets/3.Script/IDamageable.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public interface IDamageable
 {
+    //매개변수 피해량, 밀리는 힘, 맞은 위치, 맞은 각도
     public void TakeDamage(float damage, float knockBack, Vector3 hitposition, Vector3 hitNomal);
 }
