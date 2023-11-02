6c411a79bf06a896eccf4a02356ec26012c41ae8
yk170901 <yk170901@gmail.com>
2023-11-02 15:36:05 +0900

    [Feat] 더미 에너미 체력바 구현

--- Assets/1.Scene/KYS/3.Script/_Test/DummyEnemy.cs
+++ Assets/1.Scene/KYS/3.Script/_Test/DummyEnemy.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DummyEnemy : MonoBehaviour, IDamageable
 {
+    [SerializeField] private Slider hpSlider;
+
+    private float hp = 100f;
+
+    private void Awake()
+    {
+        hpSlider.maxValue = hp;
+        hpSlider.value = hp;
+    }
+
     public void TakeDamage(float damage, float knockBack, Vector3 hitposition, Vector3 hitNomal)
     {
         Debug.Log("Dummy Enemy Took Damage");
+        hp -= damage;
+        hpSlider.value = hp;
+
+        if (hp < 0)
+            Debug.Log("Enemy Died");
     }
 }
