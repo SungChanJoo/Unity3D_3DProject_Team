d6b4869b50492304a1d4d942ac799f1caf5840cc
yk170901 <yk170901@gmail.com>
2023-11-02 14:33:29 +0900

    [Add] Sword 스크립트 생성 및 Sword 프리팹에 할당.

---
+++ Assets/1.Scene/KYS/3.Script/Items/Weapons/Sword.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class Sword : MonoBehaviour, IWeapon
+{
+    public void Attack()
+    {
+
+    }
+
+    public void ChargeAttack()
+    {
+
+    }
+
+    public void Drop()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Parry()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Skill1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Skill2()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Use(PlayerData player)
+    {
+        // ChangeCurrentWeapon용으로 사용할 수도 있고,
+        // IITem에서 Use를 아예 뺄 수도 있음.
+    }
+}
