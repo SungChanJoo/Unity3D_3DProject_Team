46841c33ef3b4a6e2e5ca306f9cf9f61ff21c705
yk170901 <yk170901@gmail.com>
2023-11-02 17:47:52 +0900

    [Fix] 애니메이션 이벤트가 PlayerAttack을 부를 수 있도록 PlayerAttack 스크립트 위치 조정 & 공격 애니메이션들에 이벤트 추가

--- Assets/3.Script/Player/PlayerAttack.cs
+++ Assets/3.Script/Player/PlayerAttack.cs
@@ -11,7 +11,6 @@ public class PlayerAttack : MonoBehaviour
 
     public void OnAttackingAnimationCompleted()
     {
-        Debug.Log("OnAttackingAnimationCompleted called");
         data.CurrentWeapon.DisableDamaging();
     }
 
