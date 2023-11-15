5ae3f03b7c3455ba3a7116c93f1295e216ae1b93
yk170901 <yk170901@gmail.com>
2023-11-15 16:27:26 +0900

    [Fix] 구현한 기능들에 맞추어 씬에 넣고 설정

---
+++ Assets/1.Scene/KYS/3.Script/Characters/ConcussionEffect.cs
@@ -0,0 +1,9 @@
+﻿class ConcussionEffect : StatusEffect
+{
+    public ConcussionEffect(StatusEffectedCharacter target) : base(StatusEffectType.BlurryVision, target) { }
+    
+    public override void ApplyEffect()
+    {
+        //
+    }
+}
